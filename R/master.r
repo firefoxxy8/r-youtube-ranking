97443eae55a7cc6b3829f0571e8c88a920a8b1d8
Bror Skardhamar <bror@skardhamar.com>
2013-10-08 11:54:59 +0200

    function og model

--- R/master.r
+++ R/master.r
@@ -1,5 +1,4 @@
 # case: http://www.youtube.com/watch?v=xwndLOKQTDs
-#Hej Bror
 library(RJSONIO);
 
 library(RCurl);
@@ -9,25 +8,77 @@ library(qdap);
 library(twitteR);
 options(RCurlOptions = list(verbose = FALSE, capath = system.file("CurlSSL", "cacert.pem", package = "RCurl"), ssl.verifypeer = FALSE))
 
+#df <- NULL;
+
 youtube.video <- "xwndLOKQTDs";
-youtube.key <- "AIzaSyA8_l-irwC9QoXgymfM_N4WQuXBWCGX9bc";
-
-youtube.url <- paste("https://www.googleapis.com/youtube/v3/videos?id=", youtube.video, "&key=", youtube.key, "&fields=items(id,snippet(channelId,title,categoryId),statistics)&part=snippet,statistics", sep="")
-youtube.json <- getURL(youtube.url);
-youtube.json <- RJSONIO::fromJSON(youtube.json);
+getColumnScore <- function(youtube.video) {
+  message(youtube.video);
+  youtube.key <- "AIzaSyA8_l-irwC9QoXgymfM_N4WQuXBWCGX9bc";
+  youtube.url <- paste("https://www.googleapis.com/youtube/v3/videos?id=", youtube.video, "&key=", youtube.key, "&fields=items(id,snippet(channelId,title,categoryId),statistics)&part=snippet,statistics", sep="")
+  youtube.json <- getURL(youtube.url);
+  youtube.json <- RJSONIO::fromJSON(youtube.json);
+  stats <- youtube.json$items[[1]]$statistics;
+  snippet <- youtube.json$items[[1]]$snippet;
+  df <- data.frame("video" = youtube.video, 
+                   "youtube.title" = snippet[["title"]],
+                   "youtube.channel" = snippet[["channelId"]],
+                   "youtube.category" = snippet[["categoryId"]],
+                   "youtube.views" = as.integer(stats[["viewCount"]]),
+                   "youtube.likes" = as.integer(stats[["likeCount"]]),
+                   "youtube.dislikes" = as.integer(stats[["dislikeCount"]]),
+                   "youtube.comments" = as.integer(stats[["commentCount"]]))
+  max <- df$youtube.comments
+  batch <- 50
+  runs <- ceiling(max/batch)
+  df.comments <- NULL;
   
-stats <- youtube.json$items[[1]]$statistics;
-snippet <- youtube.json$items[[1]]$snippet;
-df <- data.frame("video" = youtube.video, 
-           "youtube.title" = snippet[["title"]],
-           "youtube.channel" = snippet[["channelId"]],
-           "youtube.category" = snippet[["categoryId"]],
-           "youtube.views" = as.integer(stats[["viewCount"]]),
-           "youtube.likes" = as.integer(stats[["likeCount"]]),
-           "youtube.dislikes" = as.integer(stats[["dislikeCount"]]),
-           "youtube.comments" = as.integer(stats[["commentCount"]]))
+  for (i in 0:(runs-1)) {
+    start.index <- i * batch + 1;
+    if (start.index <= 1000) {
+      url <- paste("https://gdata.youtube.com/feeds/api/videos/",
+                   youtube.video,
+                   "/comments?v=2&alt=json&max-results=",
+                   batch,
+                   "&start-index=",
+                   start.index, sep="");
+      
+      message(paste('trying: ', url));
+      data.url <- getURL(url);
+      tryCatch({
+        data <- RJSONIO::fromJSON(data.url);
+        mat <- matrix(data$feed$entry);
+        for (i in 1:length(mat)) {
+          entry <- data.frame(
+            comment = mat[i,][[1]]$content[[1]],
+            polarity = polarity(
+              text.var = mat[i,][[1]]$content[[1]])$all[4][[1]]
+          );
+          
+          rbind(df.comments, entry) -> df.comments
+        }
+      }, error = function(e){
+        message(paste(':(', e))
+      })
+    }
+  }
+  
+  df$youtube.sentiment <- mean(df.comments$polarity[df.comments$polarity != 0]);
+  
+  return(df);
+}
 
+# guiness, heinikein, carlsberg, bud light, budweiser, brahma, coors, corona, Sapporo Beer, Dos Equis, Hahn Super Dry
+videos <- c('xwndLOKQTDs', 'S1ZZreXEqSY', 'vs1wMp84_BA', 'ew9cEATPzDE', 'UDTZCgsZGeA', 'E1l1uXr_8vc', 'BH5izi4n7GE', 'oRdbvGgPPiw', 'K-Rs6YEZAt8', 'L-4zfsy6rsM', 'kL77IvyLF98');
+df <- NULL;
+for(i in videos) { 
+  temp <- getColumnScore(i);
+  rbind(df, temp) -> df
+} 
 
+# simple model:
+model <- lm(youtube.views ~ youtube.likes + youtube.dislikes + youtube.comments + youtube.sentiment, data = df);
+
+#### STOP HER
 # comments
 # https://gdata.youtube.com/feeds/api/videos/xwndLOKQTDs/comments?v=2&alt=json&max-results=50
 
@@ -84,7 +135,7 @@ df$youtube.sentiment <- mean(df.comments$polarity[df.comments$polarity != 0]);
 cred <- readRDS('tcred.RData')
 registerTwitterOAuth(cred);
 
-raw.twitter <- searchTwitter(youtube.video, n=1000, lang="da", retryOnRateLimit=4);
+raw.twitter <- searchTwitter(youtube.video, n=1000, lang="en", retryOnRateLimit=4);
 df.raw.twitter <- data.frame(tweet = sapply(raw.twitter, function(x) x$getText()));
 df.raw.twitter$polarity <- 0;
 for (i in 1:nrow(df.raw.twitter)) {
@@ -95,7 +146,7 @@ for (i in 1:nrow(df.raw.twitter)) {
   })
 }
 
-df$twitter.sentiment <- mean(df.raw.twitter$polarity);
+df$twitter.sentiment <- mean(df.raw.twitter$polarity[df.raw.twitter$polarity != 0]);
 
 #facebook
 # CAACEdEose0cBAKjpDrBcySEajRLfsKX2SEyLTUBAQCxlfc9hkNTnKNblcjowBkCjepLMNL85nAo9DfigZCrB3fZCF7lQkeJeHlH7L4j3ZCMiyXAdtOBwlW61wypJqsNWunZCgqcpKu0ZCUbkl2nGCTQ6WSzrGh3fiWxVjZB1pafNAohXNqZChNVK0QYwKSeS6d4RpGbXAHpmAZDZD
